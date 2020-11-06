3b67602b022c7ab87441a450b2dabfb74fc84f8b
Florian Stinglmayr <fstinglmayr@gmail.com>
2020-11-06 21:35:06 +0100

    update formatting

--- module/templates.js
+++ module/templates.js
@@ -3,14 +3,14 @@
  * Pre-loaded templates are compiled and cached for fast access when rendering
  * @return {Promise}
  */
-export const preloadHandlebarsTemplates = async function() {
-
-  // Define template paths to load
-  const templatePaths = [
-      // Attribute list partial.
-      "systems/alpha/templates/parts/sheet-attributes.html",
-      "systems/alpha/templates/parts/sheet-groups.html"
-  ];
+export const preloadHandlebarsTemplates = async function()
+{
+    // Define template paths to load
+    const templatePaths = [
+        // Attribute list partial.
+        "systems/alpha/templates/parts/sheet-attributes.html",
+        "systems/alpha/templates/parts/sheet-groups.html"
+    ];
 
     // Load the template parts
     return loadTemplates(templatePaths);
