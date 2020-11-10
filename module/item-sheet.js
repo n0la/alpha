95cf8e2bab03dca7761b2cf25ae8c07be0822aeb
Florian Stinglmayr <fstinglmayr@gmail.com>
2020-11-10 18:51:58 +0100

    fix object transformation from base to derived type

--- module/item-sheet.js
+++ module/item-sheet.js
@@ -41,7 +41,13 @@ export class AlphaItemSheet extends ItemSheet
     updateObjectType(objectType) {
         if (!(this.object instanceof objectType)) {
             let new_object = new objectType;
-            this.object = Object.assign(new_object, this.object);
+            new_object._initialize();
+            new_object.data.data = Object.assign(
+                {},
+                new_object.data.data,
+                this.object.data.data
+            );
+            this.object = new_object;
         }
     }
 }
