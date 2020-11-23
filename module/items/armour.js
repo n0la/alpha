5727b6404cf13c4da5facb664541e93d0eddd77a
Florian Stinglmayr <fstinglmayr@gmail.com>
2020-11-23 16:49:15 +0100

    formatting

--- module/items/armour.js
+++ module/items/armour.js
@@ -1,15 +1,15 @@
 import { AlphaItem } from './item.js'
 
-export class AlphaArmour extends AlphaItem {
+export class AlphaArmour extends AlphaItem
+{
+    /** @override */
+    _initialize() {
+        super._initialize();
 
-  /** @override */
-  _initialize() {
-      super._initialize();
-
-      this.prepateData({
-        rating: {value: 0},
-        hazard: {value: 0},
-        sealed: {value: false}
-    })
-  }
+        this.prepateData({
+            rating: {value: 0},
+            hazard: {value: 0},
+            sealed: {value: false}
+        })
+    }
 }
--- module/items/item.js
+++ module/items/item.js
@@ -3,49 +3,49 @@ import { AlphaArmourSheet } from "./sheets/armour.js";
 import { AlphaWeaponSheet } from "./sheets/weapon.js";
 
 export class AlphaItem extends Item {
-  /** @override */
-  _initialize() {
-      super._initialize();
+    /** @override */
+    _initialize() {
+        super._initialize();
 
-      this.prepateData({
-        resources: {value: 0},
-        value: {value: 0},
-        weight: {value: 0},
-        quantity: {value: 0},
-        description: {value: ""},
+        this.prepateData({
+            resources: {value: 0},
+            value: {value: 0},
+            weight: {value: 0},
+            quantity: {value: 0},
+            description: {value: ""},
 
-        bulky: {value: 0},
-        cumbersome: {value: false},
-        reliable: {value: false},
-        small: {value: false},
-    })
-  }
+            bulky: {value: 0},
+            cumbersome: {value: false},
+            reliable: {value: false},
+            small: {value: false},
+        })
+    }
 
-  prepateData (data) {
-    this.data.data = Object.assign(
-        {},
-        data,
-        (this.data.data || {})
-    );
-  }
+    prepateData (data) {
+        this.data.data = Object.assign(
+            {},
+            data,
+            (this.data.data || {})
+        );
+    }
 
-  get type() {
-      return super.type;
-  }
+    get type() {
+        return super.type;
+    }
 }
 
 export function RegisterItemSheets() {
-  Items.unregisterSheet("core", ItemSheet);
-  Items.registerSheet("alpha", AlphaItemSheet, {
-    types: ["item"],
-    makeDefault: true
-  });
-  Items.registerSheet("alpha", AlphaArmourSheet, {
-    types: ["armour"],
-    makeDefault: true
-  });
-  Items.registerSheet("alpha", AlphaWeaponSheet, {
-    types: ["weapon"],
-    makeDefault: true
-  });
+    Items.unregisterSheet("core", ItemSheet);
+    Items.registerSheet("alpha", AlphaItemSheet, {
+        types: ["item"],
+        makeDefault: true
+    });
+    Items.registerSheet("alpha", AlphaArmourSheet, {
+        types: ["armour"],
+        makeDefault: true
+    });
+    Items.registerSheet("alpha", AlphaWeaponSheet, {
+        types: ["weapon"],
+        makeDefault: true
+    });
 }
